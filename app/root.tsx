6f81b1471cf253a10de2c040f3aa2050c28a3377
Natt Nguyen <natt.lefevre@gmail.com>
2025-01-25 20:21:28 -0800

    setup

--- app/root.tsx
+++ app/root.tsx
@@ -6,9 +6,9 @@ import {
   Scripts,
   ScrollRestoration,
 } from "react-router";
-
 import type { Route } from "./+types/root";
 import stylesheet from "./app.css?url";
+import type { PropsWithChildren } from "react";
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -24,7 +24,7 @@ export const links: Route.LinksFunction = () => [
   { rel: "stylesheet", href: stylesheet },
 ];
 
-export function Layout({ children }: { children: React.ReactNode }) {
+export function Layout({ children }: PropsWithChildren) {
   return (
     <html lang="en">
       <head>
