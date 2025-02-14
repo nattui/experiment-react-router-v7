12138b7df169b4f55d486b329d494faed6b533b8
Natt Nguyen <natt.lefevre@gmail.com>
2025-02-14 00:47:57 -0800

    eslint

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -2,7 +2,7 @@ export function Welcome() {
   return (
     <main className="flex items-center justify-center pt-16 pb-4">
       <div className="flex min-h-0 flex-1 flex-col items-center gap-16">
-        <header className="flex flex-col items-center gap-9"></header>
+        <header className="flex flex-col items-center gap-9" />
         <div className="w-full max-w-[300px] space-y-6 px-4">
           <nav className="space-y-4 rounded-3xl border border-gray-200 p-6 dark:border-gray-700">
             <p className="text-center leading-6 text-gray-700 dark:text-gray-200">
--- eslint.config.js
+++ eslint.config.js
@@ -8,24 +8,22 @@ import tseslint from "typescript-eslint"
 /** @type {import('eslint').Linter.Config[]} */
 
 export default [
-  {
-    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
-  },
-  {
-    languageOptions: {
-      globals: globals.browser,
-    },
-  },
   ...tseslint.configs.recommended,
   js.configs.recommended,
   react.configs.flat.recommended,
   unicorn.configs["flat/all"],
   perfectionist.configs["recommended-natural"],
   {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: {
+      globals: globals.browser,
+    },
     rules: {
       "@typescript-eslint/no-empty-object-type": [
         "error",
-        { allowInterfaces: "with-single-extends" },
+        {
+          allowInterfaces: "with-single-extends",
+        },
       ],
       "perfectionist/sort-exports": "off",
       "perfectionist/sort-imports": "off",
@@ -35,7 +33,6 @@ export default [
       "unicorn/import-style": "off",
       "unicorn/no-keyword-prefix": "off",
       "unicorn/prevent-abbreviations": "off",
-      "unused-imports/no-unused-imports": "error",
     },
   },
 ]
