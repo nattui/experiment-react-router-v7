9b51f3398fa07f9572f2328e9b78c357b7074dff
Natt Nguyen <natt.lefevre@gmail.com>
2025-02-14 00:56:00 -0800

    eslint

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,11 +1,10 @@
 import { Welcome } from "../welcome/welcome"
-import type { Route } from "./+types/home"
 
 export default function Home() {
   return <Welcome />
 }
 
-export function meta({}: Route.MetaArgs) {
+export function meta() {
   return [
     { title: "New React Router App" },
     { content: "Welcome to React Router!", name: "description" },
